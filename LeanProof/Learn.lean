a5aa763b888fe6885cd88e90e8a23e627d16dc0b
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-06-22 12:56:44 -0700

    Prove commutativity of or

--- LeanProof/Learn.lean
+++ LeanProof/Learn.lean
@@ -1,4 +1,12 @@
-theorem p_and_q {p q: Prop} : p → q → p ∧ q :=
+
+variable {p q: Prop}
+
+theorem p_and_q : p → q → p ∧ q :=
   fun hp: p =>
   fun hq: q =>
-  show p ∧ q from And.intro hp hq
+  And.intro hp hq
+
+theorem commutativity_or (both: p ∨ q) : q ∨ p :=
+  Or.elim both
+    (fun (hp: p) => Or.inr hp)
+    (fun (hq: q) => Or.inl hq)
